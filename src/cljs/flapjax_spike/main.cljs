8b0a03d91f118ea00a8738c1e94aa9bbea3f9fb1
tgk <tgk@trifork.com>
2013-03-18 13:06:30 +0100

    Correctly bind up children buttons. Read maps into dom. Create new maps.

--- src/cljs/flapjax_spike/main.cljs
+++ src/cljs/flapjax_spike/main.cljs
@@ -30,6 +30,11 @@
             :request "get"
             :response "plain"}))
 
+(defn post-request [url data]
+  (clj->js {:url url
+            :request "post"
+            :response "plain"
+            :body (pr-str data)}))
 
 (defn switch-activity [main activity breast-feed nappy-change]
   (when (= :counting main)
@@ -104,33 +109,119 @@
                 (str "(_|_) Nappy-changed "
                      count "times. Changed at"
                      timestamp " by  "
-                     {:who meta} ".")
+                     (:who meta) ".")
                 "No nappy-changing done. Kid might be smelly.")]
     (dom/createDom "span" nil value)))
 
 (defn dynamicDomB [domE]
   (fj/startsWith domE (dom/createDom "span" nil "Loading ...")))
 
-(defn logE [E]
-  (fj/mapE #(.log js/console (clj->js %)) E))
+(defn logE
+  "Logs events from E to the console with the prefix s. Returns
+  an event stream which lets events from E through."
+  [s E]
+  (fj/mapE
+   (fn [e]
+     (.log js/console s (pr-str e))
+     e)
+   E))
+
+(defn logB
+  "Logs changes to B to the console with the prefix s."
+  [s B]
+  (logE s (fj/changes B)))
+
+
+(defn removeNilE
+  "Removes nil events from E."
+  [E]
+  (fj/filterE E (comp not nil?)))
+
+(defn serverModelFromB
+  "Retrieves the current server model based on the value of B.
+  request-fn is assumed to take values of B and returns a request
+  as used by restE. The initial value of the behaviour is nil.
+  Ignores nil values of B."
+  [B request-fn]
+  (fj/startsWith
+   (->> B
+        fj/changes
+        removeNilE
+        (fj/mapE request-fn)
+        restE)
+   nil))
+
+(defn children-menu-items
+  "Creates the children menu items and binds the names of clicked
+  children to E. Returns the menu items as a seq."
+  [children E]
+  (for [child children]
+    (let [elm (dom/createDom "a" (clj->js {:href "#"}) child)]
+      (fj/mapE
+       #(fj/sendEvent E child)
+       (fj/clicksE elm))
+      elm)))
+
+(defn set-children-menu
+  [children E]
+  (let [menu (dom/getElement "child-menu-content")]
+    (doseq [elm (children-menu-items children E)]
+      (dom/appendChild menu elm))))
+
+
+;; transforming state
+
+(defn feed
+  "Returns the next feed item based on the current data. Returns a fresh
+  set of data when data is nil."
+  [{count :count :as data} side]
+  (if data
+    (-> data
+        (assoc :count (inc count))
+        (assoc :timestamp 123)
+        (assoc-in [:meta :side] side))
+    {:count 1
+     :timestamp 456
+     :meta {:side side}}))
+
+(defn change
+  "Returns the next nappy change item based on the currenct data. Returns
+  a fresh set of data when data is nil."
+  [{count :count :as data} who]
+  (if data
+    (-> data
+        (assoc :count (inc count))
+        (assoc :timestamp 123)
+        (assoc-in [:meta :who] who))
+    {:count 1
+     :timestamp 456
+     :meta {:who who}}))
 
 ;; init
 
-(defn ^:export init []
+(defn ^:export init
+  []
+  (let [childrenE (restE (fj/mapE children-request))
+        switchChildE (fj/receiverE)
+        currentChildB (fj/startsWith switchChildE nil)
 
-  (let [activityE (->>
-                   (fj/getElementsByClass
-                    "menu-item"
-                    (util/elm "activity-menu"))
-                   (map fj/clicksE)
-                   (apply fj/mergeE)
-                   (fj/mapE (fn [event]
-                              (let [elm (.-toElement event)
-                                    id (.-id elm)]
-                                (id activity-map)))))
+        breastFeedB (serverModelFromB currentChildB breast-feed-request)
+        nappyChangeB (serverModelFromB currentChildB nappy-change-request)
 
-        childrenE (restE (fj/mapE children-request))
-        childMenuE (fj/mapE menu childrenE)
+        nextBreastFeedB (fj/liftB feed breastFeedB "left")
+        nextNappyChangeB (fj/liftB change nappyChangeB "Mikkel")
+
+        breastFeedDomB (fj/liftB breast-feed-dom breastFeedB)
+        nappyChageDomB (fj/liftB nappy-change-dom nappyChangeB)
+
+        activity-elms (fj/getElementsByClass "menu-item" (util/elm "activity-menu"))
+        activityE (->> activity-elms
+                       (map fj/clicksE)
+                       (apply fj/mergeE)
+                       (fj/mapE (fn [event]
+                                  (let [elm (.-toElement event)
+                                        id (.-id elm)]
+                                    (id activity-map)))))
 
 
         mainB (fj/constantB :counting)
@@ -145,8 +236,30 @@
                                         main
                                         activity
                                         breastFeedE
-                                        nappyChangeE))]
+                                        nappyChangeE))
 
+        ]
+    ;; setting up children menu
+    (fj/mapE #(set-children-menu % switchChildE) childrenE)
+
+    ;; insert content
+    (fj/insertDomB breastFeedDomB "content" "beginning")
+    (fj/insertDomB nappyChageDomB "content" "beginning")
+
+    ;; update events
+
+
+
+    ;; logging
+    #_(logB "currentChildB changes:" currentChildB)
+    #_(logB "breastFeedB:" breastFeedB)
+    #_(logB "nappyChangeB:" nappyChangeB)
+    #_(logB "activityB" activityB)
+
+    (logB "nextBreastFeedB" nextBreastFeedB)
+    (logB "nextNappyChangeB" nextNappyChangeB)
+
+    ;; old
 
     (doseq [elm (fj/getElementsByClass
                  "menu-item"
@@ -157,7 +270,7 @@
         activityB)
        elm "className"))
 
-    (fj/insertDomE
+    #_(fj/insertDomE
      (fj/mergeE
       (->> nappyChangeE
            (fj/mapE nappy-change-request)
@@ -167,14 +280,4 @@
           (fj/mapE breast-feed-request)
           restE
           (fj/mapE breast-feed-dom)))
-     "content")
-
-    (->> childrenE
-         (fj/mapE fj/clicksE)
-         logE)
-
-    (getSwitchE switchB childB switch-fn)
-
-   (fj/insertDomE
-     childMenuE
-     "child-menu-content")))
+     "content")))
